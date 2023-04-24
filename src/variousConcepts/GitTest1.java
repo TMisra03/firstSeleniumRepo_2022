6cefbe393f965d669d7435e6c761afa88887d624
TMisra03 <82480975+TMisra03@users.noreply.github.com>
2023-04-24 15:05:19 -0500

    2nd commit

---
+++ src/variousConcepts/GitTest1.java
@@ -0,0 +1,13 @@
+package variousConcepts;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git test");
+		System.out.println("Git test1");
+		System.out.println("Git test2");
+		System.out.println("Git test3");
+		System.out.println("Git test4");
+	}
+}
